25423654e5a939a6edad75f85c02e0104bccb3b6
anypanghihi <anypang_hihi@naver.com>
2022-05-14 13:00:34 +0900

    goblin archer setting

--- Capstone_2022_Japgokbap/Assets/Scripts/Monster/GoblinArcher.cs
+++ Capstone_2022_Japgokbap/Assets/Scripts/Monster/GoblinArcher.cs
@@ -1,71 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GoblinArcher : Goblins, IRangeAttackable
 {
     Coroutine m_currentCoroutine;
-    SphereCollider m_sphereCollider;
     Animator m_animator;
+    Vector3 targetPosition;
 
     [SerializeField] private float m_attackRange;
     [SerializeField] private float m_attackDelay;
 
     private void Start() 
     {
-        m_sphereCollider = GetComponent<SphereCollider>();
         m_animator = GetComponent<Animator>();
+
+        targetPosition = GameManager.instance.GetPlayerPosition();
+
+        StartCoroutine(Move());
+        //SetRange(m_attackRange);
     }
-
-    #region "Trigger Methods"
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player") && isFollowingPlayer)
-        {
-            if (m_currentCoroutine != null)
-            {
-                StopCoroutine(m_currentCoroutine);
-            }
-
-            m_currentCoroutine = StartCoroutine(RangedAttack());
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        isFollowingPlayer = true;
-        this.MyNavMesh.isStopped = false;
-        m_animator.SetTrigger("walkTrigger");
-
-        if (other.gameObject.CompareTag("Player"))
-        {
-            if (m_currentCoroutine != null)
-            {
-                StopCoroutine(m_currentCoroutine);
-            }
-
-            m_currentCoroutine = StartCoroutine(Move());
-        }
-    }
-
-    #endregion
 
     #region "Coroutine Methods"
 
     public IEnumerator RangedAttack()
     {
-        isFollowingPlayer = false;
         this.MyNavMesh.isStopped = true;
-        m_animator.SetTrigger("attackTrigger");
-        
-        while(true)
-        {
-            //실제 공격 구현
-            Debug.Log("attacking");
 
-            //attackDely 만큼 두번째 애니메이션 진행 수정
-            yield return new WaitForSeconds(m_attackDelay);
+        //실제 공격 구현
+        Vector3 dir = targetPosition - this.transform.position;
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime);
+
+        Debug.Log("attacking");
+        m_animator.SetTrigger("attackTrigger");
+        m_animator.SetBool("isAttacking", true);
+        //실제 공격 구현
+
+        yield return new WaitForSeconds(m_attackDelay);
+
+        m_animator.SetBool("isAttacking", false);
+
+        float distance = Vector3.Distance(targetPosition, this.transform.position);
+
+        if (m_attackRange >= distance)
+        {
+            m_currentCoroutine = StartCoroutine(RangedAttack());
+        }
+        else
+        {
+            isFollowingPlayer = true;
+            this.MyNavMesh.isStopped = false;
+            m_animator.SetBool("isMoving", true);
+
+            m_currentCoroutine = StartCoroutine(Move());
         }
     }
 
@@ -75,8 +63,31 @@ public class GoblinArcher : Goblins, IRangeAttackable
 
     public void SetRange(float range)
     {
-        //공격 범위에 따라 collider 크기 조절
-        m_sphereCollider.radius = range;
+        //공격 범위 조절
+        
+    }
+
+    protected override IEnumerator Move()
+    {
+        NavMesh.SamplePosition(GameManager.instance.GetPlayerPosition(), out NavMeshHit hit, 1f, 1);
+
+        this.MyNavMesh.SetDestination(hit.position);
+
+        float distance = Vector3.Distance(targetPosition, this.transform.position);
+
+        if (isFollowingPlayer && m_attackRange >= distance)
+        {
+            if (m_currentCoroutine != null)
+            {
+                StopCoroutine(m_currentCoroutine);
+            }
+
+            isFollowingPlayer = false;
+            m_animator.SetBool("isMoving", false);
+            m_currentCoroutine = StartCoroutine(RangedAttack());
+        }
+
+        yield return null;
     }
 
     #endregion
